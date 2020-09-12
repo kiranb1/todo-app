ecc68bfec591cd73498d5237f69d2021fa139b1a
kiranb1 <kayb2520@gmail.com>
2020-09-12 10:51:56 +0100

    Todos are viewable in UI

---
+++ src/components/Todo.vue
@@ -0,0 +1,34 @@
+<template>
+  <li class="list-group-item" v-bind:class="{'todo-complete':todo.complete}">
+    <input type="checkbox" v-on:change="toggleComplete" />
+    {{ todo.title }}
+    <button class="float-right" @click="$emit('delete-todo', todo.id)">
+      <i class="fas fa-trash"></i>
+    </button>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "Todo",
+  props: ["todo"],
+  methods: {
+    toggleComplete() {
+      this.todo.complete = !this.todo.complete;
+    },
+  },
+};
+</script>
+
+<style scoped>
+li {
+  width: 50%;
+  min-width: 500px;
+  margin: 0 auto;
+  color: #6c757d;
+}
+
+.todo-complete {
+  text-decoration: line-through;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div v-bind:key="todo.id" v-for="todo in todos">
+            <Todo v-bind:todo="todo"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import Todo from "@/components/Todo"
+
+export default {
+    name: "TodoList",
+    components: {
+        Todo
+    },
+    props: ["todos"]
+};
+</script>
+
+<style scoped>
+
+</style>
